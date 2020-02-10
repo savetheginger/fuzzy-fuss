0569158ee7480d97080f20ca2594c1a513d6af66
mysh <mysh995@gmail.com>
2020-02-10 13:40:11 +0000

    [RBS parser] matching rule propositions

---
+++ fuzzy_fuss/rbs/rule_base_parser.py
@@ -0,0 +1,29 @@
+import re
+
+
+class RuleBaseReader(object):
+    RULE_PATTERN = r"(?P<name>Rule\s*\d+):{0,1} (?P<body>.+)"
+
+    def __init__(self):
+        self._rules = dict()
+        self._variables = dict()
+        self._measurements = dict()
+
+    @staticmethod
+    def match_rule(line):
+        m = re.fullmatch(RuleBaseReader.RULE_PATTERN, line, re.IGNORECASE)
+        if not m:
+            return
+
+        md = m.groupdict()
+        body = md['body']
+        prop = re.findall(r"[the ]*(?P<variable>\w+) is (?P<value>\w+)", body, re.IGNORECASE)
+        connectives = [s.strip(' ') for s in re.findall(r" and | or ", body)]
+
+        return dict(name=md['name'], prop=prop, connectives=connectives)
+
+
+if __name__ == '__main__':
+    rule = r"Rule 1 If the driving is good and the journey_time is short then the tip will be big"
+    match = RuleBaseReader.match_rule(rule)
+    print(match or 'no match')
