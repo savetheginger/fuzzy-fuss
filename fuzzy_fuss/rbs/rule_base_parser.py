73ee7d29de8e2e633f8293fd1b48b06113deb653
mysh <mysh995@gmail.com>
2020-02-10 15:02:39 +0000

    [RBS parser] Matching measurement

--- fuzzy_fuss/rbs/rule_base_parser.py
+++ fuzzy_fuss/rbs/rule_base_parser.py
@@ -1,16 +1,25 @@
 import re
 
-from fuzzy_fuss.rbs.rule import Rule
+from fuzzy_fuss.rbs.rule import Rule, Atom
 
 
 class RuleBaseParser(object):
     RULE_PATTERN = r"(?P<name>Rule\s*\d+):{0,1} if (?P<propositions>.+) then (?P<conclusion>.+)"
     ATOM_PATTERN = r"(?P<variable>\w+) (?:is|will be) (?P<value>\w+)"
+    MEAS_PATTERN = r"\s*(?P<variable>\w+)\s*=\s*(?P<value>\d+(\.\d*){0,1})\s*"
 
     def __init__(self):
         self._rules = dict()
         self._variables = dict()
         self._measurements = dict()
+
+    @staticmethod
+    def match_measurement(line):
+        m = re.match(RuleBaseParser.MEAS_PATTERN, line)
+        if not m:
+            return
+        md = m.groupdict()
+        return Atom((md['variable'], float(md['value'])))
 
     @staticmethod
     def match_rule(line_raw):
@@ -36,4 +45,7 @@ class RuleBaseParser(object):
 if __name__ == '__main__':
     rl = r"Rule 1 If the driving is good and the journey_time is short then the tip will be big"
     match = RuleBaseParser.match_rule(rl)
-    print(match or 'no match')
+    print(match or 'rule: no match')
+
+    meas = "journey_time = 9"
+    print(RuleBaseParser.match_measurement(meas) or "measurement: no match")
