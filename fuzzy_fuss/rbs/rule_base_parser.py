1afc59ed220ec6045ccabc27ec04c72fe90b4c53
mysh <mysh995@gmail.com>
2020-02-10 14:33:34 +0000

    [RBS parser] Matching full rule

--- fuzzy_fuss/rbs/rule_base_parser.py
+++ fuzzy_fuss/rbs/rule_base_parser.py
@@ -3,8 +3,9 @@ import re
 from fuzzy_fuss.rbs.rule import Rule
 
 
-class RuleBaseReader(object):
-    RULE_PATTERN = r"(?P<name>Rule\s*\d+):{0,1} (?P<body>.+)"
+class RuleBaseParser(object):
+    RULE_PATTERN = r"(?P<name>Rule\s*\d+):{0,1} if (?P<propositions>.+) then (?P<conclusion>.+)"
+    ATOM_PATTERN = r"(?P<variable>\w+) (?:is|will be) (?P<value>\w+)"
 
     def __init__(self):
         self._rules = dict()
@@ -13,19 +14,22 @@ class RuleBaseReader(object):
 
     @staticmethod
     def match_rule(line):
-        m = re.fullmatch(RuleBaseReader.RULE_PATTERN, line, re.IGNORECASE)
+        line = line.replace('the ', '')
+
+        m = re.fullmatch(RuleBaseParser.RULE_PATTERN, line, re.IGNORECASE)
         if not m:
             return
 
         md = m.groupdict()
-        body = md['body']
-        prop = re.findall(r"[the ]*(?P<variable>\w+) is (?P<value>\w+)", body, re.IGNORECASE)
-        connectives = [s.strip(' ') for s in re.findall(r" and | or ", body)]
+        prop = re.findall(RuleBaseParser.ATOM_PATTERN, md['propositions'], re.IGNORECASE)
+        connectives = [s.strip(' ') for s in re.findall(r" and | or ", md['propositions'])]
+        conclusion = re.fullmatch(RuleBaseParser.ATOM_PATTERN, md['conclusion']).groups()
 
-        return Rule(name=md['name'], prop_atoms=prop, prop_connectives=connectives, conclusion=(' ', ' '))
+        rule = Rule(name=md['name'], prop_atoms=prop, prop_connectives=connectives, conclusion=conclusion)
+        return rule
 
 
 if __name__ == '__main__':
-    rule = r"Rule 1 If the driving is good and the journey_time is short then the tip will be big"
-    match = RuleBaseReader.match_rule(rule)
+    rl = r"Rule 1 If the driving is good and the journey_time is short then the tip will be big"
+    match = RuleBaseParser.match_rule(rl)
     print(match or 'no match')
