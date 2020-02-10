caae303b27783680ebc6be79c4e7014355816ced
mysh <mysh995@gmail.com>
2020-02-10 14:46:38 +0000

    [RBS parser] Atom class

--- fuzzy_fuss/rbs/rule.py
+++ fuzzy_fuss/rbs/rule.py
@@ -1,3 +1,12 @@
+class Atom(tuple):
+    def __init__(self, vals):
+        if len(vals) != 2:
+            raise ValueError(f"Expected a length 2 argument, got length {len(vals)}: {vals}")
+
+    def __repr__(self):
+        return f"({self[0]} = {self[1]})"
+
+
 class Rule(object):
     def __init__(self, name, prop_atoms, prop_connectives, conclusion):
         la = len(prop_atoms)
@@ -6,21 +15,18 @@ class Rule(object):
             raise ValueError(f"Improper number of connectives for {la} atoms: expected {la - 1}, got {lc}")
 
         self.name = name
-        self.prop_atoms = prop_atoms
+        self.prop_atoms = tuple(Atom(a) for a in prop_atoms)
         self.prop_connectives = prop_connectives
         self.conclusion = conclusion
 
     def __repr__(self):
-        def connect(atom):
-            return f"({atom[0]} = {atom[1]})"
-
         pc = self.prop_connectives
         pa = self.prop_atoms
 
-        prop = connect(pa[0])
+        prop = str(pa[0])
 
         for i in range(len(self.prop_connectives)):
             prop += f" {pc[i].upper()}"
-            prop += f" {connect(pa[i+1])}"
+            prop += f" {str(pa[i+1])}"
 
-        return f"{self.name}: {prop} => {connect(self.conclusion)}"
+        return f"{self.name}: {prop} => {str(self.conclusion)}"
