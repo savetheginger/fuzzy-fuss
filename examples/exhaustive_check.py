544f88a7a610136555e92c7222e51ebbbcc12f15
mysh <mysh995@gmail.com>
2020-02-22 21:51:31 +0000

    [Examples/plotting] Grid size instead of grid step for exhaustive check

--- examples/exhaustive_check.py
+++ examples/exhaustive_check.py
@@ -10,8 +10,8 @@ parser.add_argument('--var-indices', nargs=2, default=(0, 1), type=int,
                     help="Indices of variables for the exhaustive check (in case there is more than 2)")
 parser.add_argument('--var-grid-range', default=0.5, type=float,
                     help="Portion of variable value defining the grid range")
-parser.add_argument('--var-grid-step', default=0.05, type=float,
-                    help="Portion of variable value defining the grid step")
+parser.add_argument('--var-grid-size', default=25, type=int,
+                    help="Size of the grid (number of elements in the range)")
 parsed_args = parser.parse_args()
 
 # parse the fuzzy rule base from the file
@@ -20,26 +20,23 @@ ruleset, measurements = RuleBaseParser().parse(parsed_args.filename)
 meas_grid = {}
 for variable, value in measurements.items():
     vr = parsed_args.var_grid_range * value
-    vg = parsed_args.var_grid_step * value
-    meas_grid[variable] = np.arange(value - vr, value + vr + vg, vg)
+    meas_grid[variable] = np.linspace(value - vr, value + vr, parsed_args.var_grid_size)
 
 
 var_names = tuple(meas_grid.keys())
 i = parsed_args.var_indices
 var1 = var_names[i[0]]
 var2 = var_names[i[1]]
-vals1 = meas_grid[var1]
-vals2 = meas_grid[var2]
 
-results = np.zeros((vals1.size, vals2.size))
+results = np.zeros(2*(parsed_args.var_grid_size, ))
 
 print(f"Performing exhaustive check for {results.size} values...")
-for i1, val1 in enumerate(vals1):
-    for i2, val2 in enumerate(vals2):
+for i1, val1 in enumerate(meas_grid[var1]):
+    for i2, val2 in enumerate(meas_grid[var2]):
         results[i1, i2] = ruleset.evaluate(measurements={**measurements, var1: val1, var2: val2})
 print("Done")
 
-x, y = np.meshgrid(vals1, vals2)
+x, y = np.meshgrid(meas_grid[var1], meas_grid[var2])
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
