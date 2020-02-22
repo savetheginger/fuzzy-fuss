dd2841726253b035cb63d7b8c2edaec1c44b2eb6
mysh <mysh995@gmail.com>
2020-02-22 16:36:59 +0000

    [Examples] Exhaustive check fo

--- examples/fuzzy_system.py
+++ examples/fuzzy_system.py
@@ -15,28 +15,30 @@ parser.add_argument('--defuzz', choices=Defuzzifier.METHODS, default='coa',
 parser.add_argument('--plot', action='store_true', dest='plot', default=False,
                     help="Display plots of the parsed variables, rules, and the reasoning")
 
-parsed_args = parser.parse_args()
-kwargs = dict(composition=parsed_args.composition,
-              grid_size=parsed_args.grid_size)
+
+if __name__ == '__main__':
+    parsed_args = parser.parse_args()
+    kwargs = dict(composition=parsed_args.composition,
+                  grid_size=parsed_args.grid_size)
 
 
-# parse the fuzzy rule base from the file
-ruleset, measurements = RuleBaseParser().parse(parsed_args.filename)
+    # parse the fuzzy rule base from the file
+    ruleset, measurements = RuleBaseParser().parse(parsed_args.filename)
 
-if parsed_args.plot:
-    # plot parsed fuzzy variables
-    for fv in ruleset.variables.values():
-        fv.plot_range(margin=10)
+    if parsed_args.plot:
+        # plot parsed fuzzy variables
+        for fv in ruleset.variables.values():
+            fv.plot_range(margin=10)
 
-    # plot parsed rules
-    ruleset.plot_rules(measurements=measurements, shade=0.1, composition=parsed_args.composition)
+        # plot parsed rules
+        ruleset.plot_rules(measurements=measurements, shade=0.1, composition=parsed_args.composition)
 
-# evaluate the rule base with the measurements
-crisp_val = ruleset.evaluate(measurements, composition=parsed_args.composition, grid_size=parsed_args.grid_size,
-                             defuzz_method=parsed_args.defuzz)
-print(f"Defuzzified conclusion ('{ruleset.conclusion_names[0]}'): {crisp_val:g}")
+    # evaluate the rule base with the measurements
+    crisp_val = ruleset.evaluate(measurements, composition=parsed_args.composition, grid_size=parsed_args.grid_size,
+                                 defuzz_method=parsed_args.defuzz)
+    print(f"Defuzzified conclusion ('{ruleset.conclusion_names[0]}'): {crisp_val:g}")
 
-if parsed_args.plot:
-    # plot the evaluation procedure
-    weights = ruleset.compute_weights(measurements)
-    ruleset.plot_eval(weights, composition=parsed_args.composition, crisp_conclusion=crisp_val)
+    if parsed_args.plot:
+        # plot the evaluation procedure
+        weights = ruleset.compute_weights(measurements)
+        ruleset.plot_eval(weights, composition=parsed_args.composition, crisp_conclusion=crisp_val)
